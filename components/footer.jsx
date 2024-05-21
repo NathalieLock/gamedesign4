4bff5199f6c5c8af538716b705f4443ad47d4958
NathalieLock <144237311+NathalieLock@users.noreply.github.com>
2024-05-21 15:01:28 +0200

    add info about course

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function Footer() {
-  return <div className="h-[20rem]"></div>;
+  return (
+    <div className="max-w-[50rem] h-[20rem] mx-auto flex items-center leading-6 relative">
+      <p className="absolute bottom-0 left-1/2 transform -translate-x-1/2 pb-[2rem] text-center text-sm">
+        This project is part of the course DH2650 Computer Game Design at KTH,
+        May 2024.
+      </p>
+    </div>
+  );
 }
