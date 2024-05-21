7c09620e01a393e7d06851d80a5358de785e0ea8
NathalieLock <144237311+NathalieLock@users.noreply.github.com>
2024-05-21 16:12:27 +0200

    style features component

--- components/features.jsx
+++ components/features.jsx
@@ -3,24 +3,49 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
+import { features } from "@/lib/data";
+
+const fadeInAnimationVariants = {
+  initial: {
+    opacity: 0,
+    y: 100,
+  },
+  animate: (index) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.05 * index,
+    },
+  }),
+};
 
 export default function Features() {
   const { ref } = useSectionInView("Features", 0.6);
 
   return (
-    <motion.section
+    <section
+      id="features"
       ref={ref}
       className="max-w-[50rem] mt-[10rem] text-center leading-8 scroll-mt-28"
-      initial={{ opacity: 0, y: 100 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ delay: 0.175 }}
-      id="features"
     >
       <h2 className="text-3xl">Features</h2>
-
-      <p className="max-w-[50rem] mx-auto pt-[2rem] px-[2rem] leading-loose">
-        yoyo
-      </p>
-    </motion.section>
+      <ul className="flex flex-wrap justify-center gap-4 md:gap-6 lg:gap-8 text-lg font-medium pt-6 lg:pt-8">
+        {features.map((feature, index) => (
+          <motion.li
+            className="bg-pink-600 inline-block rounded-xl px-4 py-2"
+            key={index}
+            variants={fadeInAnimationVariants}
+            initial="initial"
+            whileInView="animate"
+            viewport={{
+              once: true,
+            }}
+            custom={index}
+          >
+            {feature}
+          </motion.li>
+        ))}
+      </ul>
+    </section>
   );
 }
