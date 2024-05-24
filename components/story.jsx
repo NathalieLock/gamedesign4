18df03e43bc53781a5a523c4988ef26fee973c0c
NathalieLock <144237311+NathalieLock@users.noreply.github.com>
2024-05-25 00:48:41 +0200

    update story

--- components/story.jsx
+++ components/story.jsx
@@ -19,7 +19,20 @@ export default function Story() {
       <h2 className="text-3xl">Story</h2>
 
       <p className="max-w-[50rem] mx-auto pt-[2rem] px-[2rem] leading-loose">
-        Robot boy blablablablabla
+        The adventure starts when you, playing as Robo the robot, spawn into a
+        mystical and vibrant world. As you arrive, you find out that you stand
+        at the threshold of a realm shrouded in enchantment and peril. The once
+        serene lands are now overshadowed by a creeping darkness, brought on by
+        a malevolent force fueled by hallucinatory spores. The inhabitants of
+        this world are in desperate need of salvation. Determined to uncover the
+        mystery and restore peace, you embark on a quest filled with exploration
+        and puzzle-solving. Each step brings you closer to uncovering the
+        secrets of the world and the source of the spores. Along the way, you’ll
+        meet otherworldly creatures, each with their own tales and quests, and
+        gain allies in your fight against the dark forces. Will you be able to
+        navigate the challenges, solve the puzzles, and defeat the enemy ? The
+        fate of the otherworld realm rests in your hands. Embrace your role as
+        their guardian and forge a path to victory.
       </p>
     </motion.section>
   );
