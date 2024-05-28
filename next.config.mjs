8b4f66be6379fd4d5ed8881f4387e13f1d668d8d
NathalieLock <144237311+NathalieLock@users.noreply.github.com>
2024-05-29 00:06:26 +0200

    update next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,29 @@
 /** @type {import('next').NextConfig} */
 import { withNextVideo } from "next-video/process";
+import path from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
 
-const nextConfig = {}; // Your current Next Config object
+// Get the directory name in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Ensure the directory exists
+const ensureDirectoryExistence = (dirPath) => {
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath, { recursive: true });
+  }
+};
+
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    // Ensure the directory exists on the client-side build
+    if (!isServer) {
+      ensureDirectoryExistence(path.join(__dirname, "public/_next-video"));
+    }
+    return config;
+  },
+};
 
 export default withNextVideo(nextConfig);
