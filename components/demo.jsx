bcae6e3b639f863355002d1bbbc7ca53fcba8415
NathalieLock <144237311+NathalieLock@users.noreply.github.com>
2024-05-29 00:33:31 +0200

    fix back

--- components/demo.jsx
+++ components/demo.jsx
@@ -3,7 +3,6 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
-import VideoDemo from "./video";
 
 export default function Demo() {
   const { ref } = useSectionInView("Demo", 0.6);
@@ -18,7 +17,7 @@ export default function Demo() {
     >
       <h2 className="text-3xl">Video demo</h2>
       <div className="aspect-video mt-[2rem]">
-        <VideoDemo />
+        <div className="w-full h-full bg-slate-500"></div>
       </div>
     </motion.section>
   );
--- components/video.jsx
+++
@@ -1,6 +0,0 @@
-import Video from "next-video";
-import awesomeVideo from "https://axk3pvjzeu6rv0tu.public.blob.vercel-storage.com/Otherworld%20Guardian-TkFsi7gzmOv43YCTUqzZUYgLMcLGWB.mp4";
-
-export default function VideoDemo() {
-  return <Video src={awesomeVideo} />;
-}
