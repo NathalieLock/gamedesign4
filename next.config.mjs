0687c7c09921fb5817bbca381ed37774572fbe23
NathalieLock <144237311+NathalieLock@users.noreply.github.com>
2024-05-29 00:10:14 +0200

    Revert "update next.config"
    This reverts commit 8b4f66be6379fd4d5ed8881f4387e13f1d668d8d.

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,6 @@
 /** @type {import('next').NextConfig} */
 import { withNextVideo } from "next-video/process";
-import path from "path";
-import { fileURLToPath } from "url";
-import fs from "fs";
 
-// Get the directory name in ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Ensure the directory exists
-const ensureDirectoryExistence = (dirPath) => {
-  if (!fs.existsSync(dirPath)) {
-    fs.mkdirSync(dirPath, { recursive: true });
-  }
-};
-
-const nextConfig = {
-  reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    // Ensure the directory exists on the client-side build
-    if (!isServer) {
-      ensureDirectoryExistence(path.join(__dirname, "public/_next-video"));
-    }
-    return config;
-  },
-};
+const nextConfig = {}; // Your current Next Config object
 
 export default withNextVideo(nextConfig);
