91e8ca744f2dc62eb10af17894e04a7231cb5875
NathalieLock <144237311+NathalieLock@users.noreply.github.com>
2024-05-31 13:25:11 +0200

    update video demo

--- components/demo.jsx
+++ components/demo.jsx
@@ -16,10 +16,10 @@ export default function Demo() {
       id="demo"
     >
       <h2 className="text-3xl">Video demo</h2>
-      <div className="aspect-video pt-6 lg:pt-8">
+      <div className="pt-6 lg:pt-8">
         <video className="h-full w-full rounded-lg" controls>
           <source
-            src="https://axk3pvjzeu6rv0tu.public.blob.vercel-storage.com/demo-Pe9ZETEoPctb7n6aQNcK9iNDgF0Lhw.mp4"
+            src="https://axk3pvjzeu6rv0tu.public.blob.vercel-storage.com/demo2-JNSwH7pce506jCzogU2SJQDGcn542g.mp4"
             type="video/mp4"
           />
           Your browser does not support the video tag.
