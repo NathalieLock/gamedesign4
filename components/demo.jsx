1b708776ef7b369bfc79cf384a9ea32422d0a892
NathalieLock <144237311+NathalieLock@users.noreply.github.com>
2024-05-31 13:57:39 +0200

    update video with more detailed music credit

--- components/demo.jsx
+++ components/demo.jsx
@@ -19,7 +19,7 @@ export default function Demo() {
       <div className="pt-6 lg:pt-8">
         <video className="h-full w-full rounded-lg" controls>
           <source
-            src="https://axk3pvjzeu6rv0tu.public.blob.vercel-storage.com/demo2-JNSwH7pce506jCzogU2SJQDGcn542g.mp4"
+            src="https://axk3pvjzeu6rv0tu.public.blob.vercel-storage.com/demo3-KiiyXrl3lZK2RkuaIyqkQ95MbIeWX7.mp4"
             type="video/mp4"
           />
           Your browser does not support the video tag.
