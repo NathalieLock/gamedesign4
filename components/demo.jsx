59a9335433a980019d60699406f626d94073bc34
NathalieLock <144237311+NathalieLock@users.noreply.github.com>
2024-06-13 17:37:09 +0200

    Update video demo to youtube video
    The free space used for storing the video at Vercel Blob had been used up
    
    Needed to embed the video from youtube instead

--- components/demo.jsx
+++ components/demo.jsx
@@ -4,27 +4,38 @@ import React from "react";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
 
+/*
+ <div className="pt-6 lg:pt-8">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/f9A5qrTTP8Y?si=3byhBTAu8WLhWavo"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
+      </div>
+      */
+
 export default function Demo() {
   const { ref } = useSectionInView("Demo", 0.6);
   return (
     <motion.section
       ref={ref}
-      className="max-w-[45rem] text-center leading-8 scroll-mt-28"
+      className="text-center leading-8 scroll-mt-28"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
       id="demo"
     >
       <h2 className="text-3xl">Video demo</h2>
-      <div className="pt-6 lg:pt-8">
-        <video className="h-full w-full rounded-lg" controls>
-          <source
-            src="https://axk3pvjzeu6rv0tu.public.blob.vercel-storage.com/demo3-KiiyXrl3lZK2RkuaIyqkQ95MbIeWX7.mp4"
-            type="video/mp4"
-          />
-          Your browser does not support the video tag.
-        </video>
-      </div>
+      <iframe
+        className="w-[20rem] md:w-[35rem] lg:w-[45rem] aspect-video pt-6 lg:pt-8"
+        src="https://www.youtube.com/embed/f9A5qrTTP8Y?si=3byhBTAu8WLhWavo"
+        allow="fullscreen"
+      ></iframe>
     </motion.section>
   );
 }
